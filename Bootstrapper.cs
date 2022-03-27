a76e172a326c063e5158db9d5d14a0df0dcb85ba
bajrangsingh <bajrangsingh@gmail.com>
2022-03-26 23:38:26 -0400

    Add files via upload

---
+++ Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using Unity.Mvc4;
+using ApprovalPortal.Repository;
+
+namespace ApprovalPortal
+{
+    public static class Bootstrapper
+    {
+        public static IUnityContainer Initialise()
+        {
+            var container = BuildUnityContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            return container;
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+
+            RegisterTypes(container);
+            return container;
+        }
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.RegisterType<IApprovalRepo, ApprovalRepo>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApprovalPortal.MvcApplication" Language="C#" %>
